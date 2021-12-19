50efb8d7ff3211fc61ae63d38b0c0cb38035d1b8
Junjae Lee <ejj612o@gmail.com>
2021-12-19 22:53:58 +0900

    first commit

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Sensor from "./component/sensor.svelte";
+
+    function addDataPoint() {
+        chartRef.addDataPoint("Wed", [Math.random() * 20]);
+
+        charRef1.removeDataPoint(0);
+        charRef1.addDataPoint("wed", [Math.random() * 20]);
+    }
+</script>
+
+<h1>고장 진단 시스템</h1>
+<div class="sensorContainer">
+    <Sensor sensorNum="1" />
+    <Sensor sensorNum="2" />
+    <Sensor sensorNum="3" />
+    <Sensor sensorNum="4" />
+</div>
+
+<button on:click={addDataPoint}>Add data point</button>
+
+<style>
+    .sensorContainer {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+</style>
---
+++ my-svelte-project/src/component/sensor.svelte
@@ -0,0 +1,41 @@
+<script>
+    import Chart from "svelte-frappe-charts";
+    export let sensorNum = 0;
+
+    let data = {
+        labels: [
+            "Sun",
+            "Mon",
+            "Tues",
+            "Wed",
+            "Thurs",
+            "Fri",
+            "Sat",
+            "Sun",
+            "Mon",
+            "Tues",
+            "Wed",
+            "Thurs",
+            "Fri",
+            "Sat",
+        ],
+        datasets: [
+            {
+                values: [10, 12, 3, 9, 8, 15, 9, 10, 12, 3, 9, 8, 15, 9],
+            },
+        ],
+    };
+
+    export let chartRefRumble;
+    export let charRefTemperature;
+</script>
+
+<div>
+    <h2>
+        {sensorNum}번 센서
+    </h2>
+    <h3>진동</h3>
+    <Chart {data} type="line" bind:this={chartRefRumble} />
+    <h3>온도</h3>
+    <Chart {data} type="line" bind:this={charRefTemperature} />
+</div>
