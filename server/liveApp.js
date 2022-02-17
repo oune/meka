72f5667352154f66ab3bfac9129130a784a6d15a
Junjae Lee <ejj612o@gmail.com>
2022-02-17 16:48:45 +0900

    코드 정리

--- server/liveApp.js
+++ server/liveApp.js
@@ -40,24 +40,26 @@ var server = net.createServer(function (client) {
     console.log('Client connection: ');
     console.log('   local = %s:%s', client.localAddress, client.localPort);
     console.log('   remote = %s:%s', client.remoteAddress, client.remotePort);
+
     client.setTimeout(500);
     client.setEncoding('utf8');
+
     client.on('data', function (data) {
-        console.log('Received data from client on port %d: %s',
-            client.remotePort, data.toString());
+        console.log('Received data from client on port %d: %s', client.remotePort, data.toString());
         console.log('  Bytes received: ' + client.bytesRead);
-        writeData(client, 'Sending: ' + data.toString());
-        console.log('  Bytes sent: ' + client.bytesWritten);
     });
+
     client.on('end', function () {
         console.log('Client disconnected');
         server.getConnections(function (err, count) {
             console.log('Remaining Connections: ' + count);
         });
     });
+
     client.on('error', function (err) {
         console.log('Socket Error: ', JSON.stringify(err));
     });
+
     client.on('timeout', function () {
         console.log('Socket Timed out');
     });
@@ -71,13 +73,3 @@ server.listen(3001, function () {
         console.log('Server Error: ', JSON.stringify(err));
     });
 });
-function writeData(socket, data) {
-    var success = !socket.write(data);
-    if (!success) {
-        (function (socket, data) {
-            socket.once('drain', function () {
-                writeData(socket, data);
-            });
-        })(socket, data);
-    }
-}
