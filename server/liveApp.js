2dccbfad323e1306dffcc4c01a82144dd07e7121
Junjae Lee <ejj612o@gmail.com>
2022-02-17 16:53:05 +0900

    인코딩 안시로 변경

--- server/liveApp.js
+++ server/liveApp.js
@@ -42,7 +42,7 @@ var server = net.createServer(function (client) {
     console.log('   remote = %s:%s', client.remoteAddress, client.remotePort);
 
     client.setTimeout(500);
-    client.setEncoding('utf8');
+    client.setEncoding('ansi');
 
     client.on('data', function (data) {
         console.log('Received data from client on port %d: %s', client.remotePort, data.toString());
