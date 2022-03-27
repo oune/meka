1d298bc2254a6527e089290c1ccb75059563c271
Junjae Lee <ejj612o@gmail.com>
2022-03-27 15:02:39 +0900

    센서 번호 수정

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,16 +1,13 @@
 <script>
     import Sensor from "./component/sensor.svelte";
-    import Demo from "./component/test.svelte";
 </script>
 
 <h1>고장 진단 시스템</h1>
 <div class="sensorContainer">
+    <Sensor sensorNum="0" />
     <Sensor sensorNum="1" />
     <Sensor sensorNum="2" />
     <Sensor sensorNum="3" />
-    <Sensor sensorNum="4" />
-
-    <Demo />
 </div>
 
 <style>
