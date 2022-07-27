1b33c6bcffee1e6da82c85bff25662ea04149ba1
Junjae Lee <ejj612o@gmail.com>
2022-07-27 15:59:35 +0900

    컴포넌트 통합

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -7,10 +7,10 @@
 <body>
     <h1>고장 진단 시스템</h1>
     <div class="sensorContainer">
-        <Block sensorNum="1" vibrationPort="3001" />
-        <Block sensorNum="2" vibrationPort="3002" />
-        <Block sensorNum="3" vibrationPort="3003" />
-        <Block sensorNum="4" vibrationPort="3004" />
+        <Block sensorNum="1" port="3001" />
+        <Block sensorNum="2" port="3002" />
+        <Block sensorNum="3" port="3003" />
+        <Block sensorNum="4" port="3004" />
     </div>
 </body>
 
