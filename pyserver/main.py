cb99209798c87fd84ad329bbafc364ae6a802fa4
Junjae Lee <ejj612o@gmail.com>
2022-07-29 16:39:29 +0900

    gui 추가

--- pyserver/main.py
+++ pyserver/main.py
@@ -1,7 +1,8 @@
 from typing import List
-from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
+import sys
+from PyQt5.QtWidgets import *
 
 from sensor import Sensor
 import config
@@ -13,26 +14,37 @@ sensor = Sensor.of(device_name, device_channel_name,
                    sampling_rate, samples_per_channel, type)
 
 
-async def detect_pump(data: List[float]):
-    return ""
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('고장 진단 시스템')
+        self.resize(400, 200)
+
+        device_select = QComboBox(self)
+        device_select.addItem('device a')
+        device_select.addItem('device b')
+        device_select.addItem('device c')
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(device_select)
+        hbox.addStretch(1)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+        self.show()
 
 
-async def detect_pump(data: List[float]):
-    return ""
-
-app = FastAPI()
-
-
-@app.get("/")
-async def get_webpage():
-    return ""
-
-
-@app.get("/config")
-async def get_config():
-    return ""
-
-
-@app.put("/config")
-async def update_config(data):
-    return ""
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
