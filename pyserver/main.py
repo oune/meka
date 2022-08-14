46214273b16839158f6d15a6e9f06b5aa0bfa837
Junjae Lee <ejj612o@gmail.com>
2022-08-14 17:11:41 +0900

    gui 제거

--- pyserver/main.py
+++ pyserver/main.py
@@ -1,7 +1,6 @@
 from typing import List
 from pydantic import BaseModel
 import pandas as pd
-import sys
 from PyQt5.QtWidgets import *
 
 from sensor import Sensor
@@ -12,39 +11,3 @@ device_name, device_channel_name, sampling_rate, samples_per_channel, type = con
 
 sensor = Sensor.of(device_name, device_channel_name,
                    sampling_rate, samples_per_channel, type)
-
-
-class MyApp(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('고장 진단 시스템')
-        self.resize(400, 200)
-
-        device_select = QComboBox(self)
-        device_select.addItem('device a')
-        device_select.addItem('device b')
-        device_select.addItem('device c')
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(device_select)
-        hbox.addStretch(1)
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
-
-        self.setLayout(vbox)
-
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyApp()
-    sys.exit(app.exec_())
