3760f3f24cf37ce1d1c119dd6a86aa602b64fa16
Junjae Lee <ejj612o@gmail.com>
2022-09-19 15:13:30 +0900

    설계 및 틀잡기

--- pyserver/main.py
+++ pyserver/main.py
@@ -1,7 +1,8 @@
 from typing import List
 from pydantic import BaseModel
 import pandas as pd
-from PyQt5.QtWidgets import *
+import socketio
+import asyncio
 
 from sensor import Sensor
 import config
@@ -11,3 +12,20 @@ device_name, device_channel_name, sampling_rate, samples_per_channel, type = con
 
 sensor = Sensor.of(device_name, device_channel_name,
                    sampling_rate, samples_per_channel, type)
+
+sio = socketio.AsyncServer()
+
+
+@sio.on('setting')
+def another_event(sid, data):
+    pass
+
+
+async def roop():
+    datas = await sensor.read()
+    sio.emit('data', {'data': datas})
+    # TODO req model
+    # TODO emit 'model_res'
+
+
+asyncio.run(roop())
