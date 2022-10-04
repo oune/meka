15cf0e891c72878b962ab017f97da009cf298bf6
Junjae Lee <ejj612o@gmail.com>
2022-10-04 14:03:27 +0900

    재준이형 파일 통합

---
+++ pyserver/Inference/ae_LSTM.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.layers import Input, LSTM, RepeatVector, TimeDistributed, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras import regularizers
+
+## input_shape(n, n)
+def autoencoder_model(input_shape : tuple):
+    inputs = Input(shape=input_shape)
+    L1 = LSTM(64, activation='relu', return_sequences=True,
+            kernel_regularizer=regularizers.l2(0.00))(inputs)
+    L2 = LSTM(32, activation='relu', return_sequences=False)(L1)
+    L3 = RepeatVector(input_shape[0])(L2)
+    L4 = LSTM(32, activation='relu', return_sequences=True)(L3)
+    L5 = LSTM(64, activation='relu', return_sequences=True)(L4)
+
+    output = TimeDistributed(Dense(input_shape[1]))(L5)    
+    model = Model(inputs=inputs, outputs=output)
+
+    return model
---
+++ pyserver/Inference/inference.py
@@ -0,0 +1,31 @@
+import numpy as np
+import tensorflow as tf
+import joblib
+from ae_LSTM import autoencoder_model
+
+
+model_path = r''  ## model.h5 path
+scaler_path = r''  ## StandardScaler -> pipeline.pkl path
+input_shape = (1, 4)
+threshold = 0.000833  ## 유동적으로 지정
+
+scaler = joblib.load(scaler_path)  ## load pipeline.pkl
+model = autoencoder_model(input_shape)
+model = tf.keras.models.load_model(model_path)  ## load model.h5
+
+def inference(model, data, scaler, threshold):
+    data_0, data_1, data_2, data_3 = data
+
+    data_0 = scaler.transform(np.mean(np.abs(data_0), axis=0))
+    data_1 = scaler.transform(np.mean(np.abs(data_1), axis=0))
+    data_2 = scaler.transform(np.mean(np.abs(data_2), axis=0))
+    data_3 = scaler.transform(np.mean(np.abs(data_3), axis=0))
+
+    test_data = np.stack([data_0, data_1, data_2, data_3])
+    test_data = test_data.reshape(1, 1, 4)
+
+    output = model(test_data).numpy().reshape(1, 4)
+
+    output_mse = np.mean(np.power(test_data.reshape(1, 4) - output, 2), axis=1)
+
+    return (output_mse > threshold).item()
