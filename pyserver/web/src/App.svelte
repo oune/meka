bb9f96524db47a007bb95228fa21ca26c19a2118
Junjae Lee <ejj612o@gmail.com>
2022-10-06 10:46:52 +0900

    로딩 버튼 디자인 변경, 서버 재접속 추가

--- pyserver/web/src/App.svelte
+++ pyserver/web/src/App.svelte
@@ -1,16 +1,23 @@
 <script>
     import Chart from "svelte-frappe-charts";
     import { io } from "socket.io-client";
+    import { Jumper } from "svelte-loading-spinners";
 
     Notification.requestPermission();
 
     let model_res = true;
+    let isReloading = true;
 
     const url = `http://localhost:8000`;
     const socket = io(url);
 
     socket.on("connect", () => {
         console.log(`sensor connected on id: ${socket.id}`);
+        isReloading = false;
+    });
+
+    socket.on("connect_error", () => {
+        isReloading = true;
     });
 
     socket.on("data", (arg) => {
@@ -71,12 +78,22 @@
 </script>
 
 <body>
-    <h1>고장 진단 시스템</h1>
+    <div id="header">
+        <h1>고장 진단 시스템</h1>
+        <h2 id="reload">
+            {#if isReloading}
+                <Jumper size="60" color="#004fff" unit="px" duration="1s" />
+                서버 접속중...
+            {/if}
+        </h2>
+    </div>
     <h2>
-        상태 : {#if model_res == true}
+        상태 : {#if isReloading}
+            🌀 연결 끊김
+        {:else if model_res}
             ✅ 정상
         {:else}
-            ❗이상 감지됨
+            ❌ 이상 감지됨
         {/if}
     </h2>
     <Chart
@@ -95,4 +112,41 @@
 </body>
 
 <style>
+    #header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    #reload {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    h2 {
+        margin: 0;
+    }
+
+    button {
+        width: 120px;
+        height: 40px;
+        color: #fff;
+        background: #004fff;
+        font-size: 16px;
+        border: none;
+        border-radius: 20px;
+        box-shadow: 0 4px 16px rgba(0, 79, 255, 0.3);
+        transition: 0.3s;
+        left: 50%;
+        top: 50%;
+    }
+    button:focus {
+        outline: 0;
+    }
+    button:hover {
+        background: rgba(0, 79, 255, 0.9);
+        cursor: pointer;
+        box-shadow: 0 2px 4px rgba(0, 79, 255, 0.6);
+    }
 </style>
