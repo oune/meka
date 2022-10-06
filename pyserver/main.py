30408109bcc2437dbd6176ead1ad2d8efc4bf27c
Junjae Lee <ejj612o@gmail.com>
2022-10-06 10:46:24 +0900

    서버 센서 예외처리

--- pyserver/main.py
+++ pyserver/main.py
@@ -3,17 +3,23 @@ from pydantic import BaseModel
 from sensor import Sensor
 from time import ctime, time
 from Inference.inference import inference, mse, model, scaler, threshold
+from sys import exit
 
+import nidaqmx
 import socketio
 import config
 
 device_name, device_channel_name, sampling_rate, samples_per_channel, modeltype = config.load(
     'config.ini')
 
-sensor = Sensor.of(device_name,
-                   device_channel_name,
-                   sampling_rate,
-                   samples_per_channel * 2, modeltype)
+try:
+    sensor = Sensor.of(device_name,
+                       device_channel_name,
+                       sampling_rate,
+                       samples_per_channel * 2, modeltype)
+except nidaqmx.errors.DaqError:
+    print('잘못된 설정값이 입력 되었습니다. config.ini 파일을 올바르게 수정해 주세요.')
+    exit()
 
 
 async def loop():
@@ -21,6 +27,7 @@ async def loop():
         datas = await sensor.read(samples_per_channel, 30.0)
         now_time = ctime(time())
 
+        await sio.sleep(1)
         for idx in range(0, len(datas)):
             await sio.emit('data', {'sensor_id': idx, 'time': now_time})
             await sio.sleep(1)
